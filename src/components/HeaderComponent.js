e9fe3a80a52d69dfe7ad6612a10a9c37f731b5c8
Claire Lu <appsfunlu@gmail.com>
2021-12-16 10:34:07 -0800

    added resume

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,60 +1,69 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export default class Header extends Component {
   render() {
     return (
       <React.Fragment>
         <header
-          className="sticky-top"
+          className='sticky-top'
           style={{
-            backgroundColor: "#000000",
-            textColor: "#ffffff",
+            backgroundColor: '#000000',
+            textColor: '#ffffff',
             fontSize: 20,
-            padding:10,
+            padding: 10,
           }}
         >
-          <nav id="nav-wrap sticky-top ">
-            <ul id="nav" className="nav">
-              <li className="current">
+          <nav id='nav-wrap sticky-top '>
+            <ul id='nav' className='nav'>
+              <li className='current'>
                 <a
-                  className="m-4"
-                  style={{ backgroundColor: "#000000", color: "#ffffff" }}
-                  href="#home"
+                  className='m-4'
+                  style={{ backgroundColor: '#000000', color: '#ffffff' }}
+                  href='#home'
                 >
                   Home
                 </a>
               </li>
               <li>
                 <a
-                  className="mr-4"
-                  style={{ backgroundColor: "#000000", color: "#ffffff" }}
-                  href="#skill"
+                  className='mr-4'
+                  style={{ backgroundColor: '#000000', color: '#ffffff' }}
+                  href='#skill'
                 >
                   Skills
                 </a>
               </li>
               <li>
                 <a
-                  className="mr-4"
-                  style={{ backgroundColor: "#000000", color: "#ffffff" }}
-                  href="#project"
+                  className='mr-4'
+                  style={{ backgroundColor: '#000000', color: '#ffffff' }}
+                  href='#project'
                 >
                   Projects
                 </a>
               </li>
               <li>
                 <a
-
-                  style={{ backgroundColor: "#000000", color: "#ffffff" }}
-                  href="#contact"
+                  className='mr-4'
+                  style={{ backgroundColor: '#000000', color: '#ffffff' }}
+                  href='#contact'
                 >
                   Contact
+                </a>
+              </li>
+              <li>
+                <a
+                  className='mr-4'
+                  style={{ backgroundColor: '#000000', color: '#ffffff' }}
+                  href='https://drive.google.com/file/d/1hM7MMuX8Tcdj5I44tITJhR47O7Oa4AOy/view?usp=sharing'
+                >
+                  Resume
                 </a>
               </li>
             </ul>
           </nav>
         </header>
       </React.Fragment>
-    );
+    )
   }
 }
